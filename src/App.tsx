538cfa207eda79183349565a0107beacbda158ce
Stephen Kurtz <stephen.kurtz@morganlewis.com>
2024-10-22 16:11:16 -0400

    import component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { FoodDeliveryForm } from './components/FoodDeliveryForm'
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
       <div className="container">
         <div className="mx-5">
           <div>
-            food delivery form
+            <FoodDeliveryForm />
           </div>
         </div>
       </div>
