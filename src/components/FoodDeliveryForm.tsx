f4dea0d4c54576953a1d3eb914a2660d03f59bf1
Stephen Kurtz <stephen.kurtz@morganlewis.com>
2024-10-23 10:57:56 -0400

    pass default values to entire form

--- src/components/FoodDeliveryForm.tsx
+++ src/components/FoodDeliveryForm.tsx
@@ -2,6 +2,8 @@ import { useForm } from 'react-hook-form'
 import { getRenderCount } from '../utils/getRenderCount';
 
 type FoodDeliveryFormType = {
+    orderNumber: number,
+    email: string,
     customerName: string,
     mobile: string,
 }
@@ -9,7 +11,14 @@ type FoodDeliveryFormType = {
 const RenderCount = getRenderCount();
 
 export const FoodDeliveryForm = () => {
-    const { register, handleSubmit } = useForm<FoodDeliveryFormType>();
+    const { register, handleSubmit } = useForm<FoodDeliveryFormType>({
+        defaultValues: {
+            orderNumber: new Date().valueOf(),
+            email: '',
+            customerName: '',
+            mobile: '',
+        }
+    });
 
     const onSubmit = (formData: FoodDeliveryFormType) => {
         console.log('form data: ', formData);
@@ -18,23 +27,54 @@ export const FoodDeliveryForm = () => {
     return (
         <form autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
             <RenderCount />
-            <div className="form-floating mb-3">
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Customer Name"
-                    {...register('customerName')}
-                />
-                <label>Customer Name</label>
+            <div className="row mb-2">
+                <div className="col">
+                    <div className="form-floating">
+                        <input
+                            type="text"
+                            className="form-control"
+                            disabled
+                            placeholder="Order Number"
+                            {...register('orderNumber')}
+                        />
+                        <label>Order Number</label>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="form-floating">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Mobile"
+                            {...register('mobile')}
+                        />
+                        <label>Mobile</label>
+                    </div>
+                </div>
             </div>
-            <div className="form-floating mb-3">
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Mobile"
-                    {...register('mobile')}
-                />
-                <label>Mobile</label>
+            <div className="row mb-2">
+                <div className='col'>
+                    <div className="form-floating">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Customer Name"
+                            {...register('customerName')}
+                        />
+                        <label>Customer Name</label>
+                    </div>
+                </div>
+                <div className='col'>
+                    <div className="form-floating">
+                        <input
+                            type="email"
+                            className="form-control"
+                            placeholder="Email"
+                            {...register('email')}
+                        />
+                        <label>Email</label>
+                    </div>
+                </div>
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
