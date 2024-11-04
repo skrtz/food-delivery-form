44556a0c981fcef96a7e2be72ecb6f1ec2142b0e
Stephen Kurtz <stephen.kurtz@morganlewis.com>
2024-11-04 17:04:27 -0500

    create reusable submit button component

---
+++ src/controls/SubmitButton.tsx
@@ -0,0 +1,34 @@
+
+type SubmitButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    isSubmitting?: boolean
+}
+
+export const SubmitButton = (props: SubmitButtonProps) => {
+    const {
+        isSubmitting = undefined,
+        className = 'btn-light',
+        value,
+        ...other
+    } = props
+
+    return (
+        <button
+            type="submit"
+            className={`btn ${className}`}
+            disabled={isSubmitting == undefined ? false : isSubmitting}
+            {...other}
+        >
+            {isSubmitting === undefined || isSubmitting === false ? (
+                value
+            ) : (
+                <>
+                    <span
+                        className="spinner-border spinner-border-sm"
+                        aria-hidden="true">
+                    </span>
+                    <span role="status" className="ms-1">{value}</span>
+                </>
+            )}
+        </button>
+    )
+}
